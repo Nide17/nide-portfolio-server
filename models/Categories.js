dafd52d9438e15df88aa63b35d59cbadf9a182c8
Nide17 <nidedrogba@gmail.com>
2023-10-07 20:11:15 +0200

    db update to render ok

--- models/Categories.js
+++ models/Categories.js
@@ -3,10 +3,23 @@ const Sequelize = require('sequelize')
 const db = require('../config/database')
 
 const Categories = db.define('categories', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     title: {
         type: Sequelize.STRING,
         allowNull: false
-    }
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    },
 })
 
 module.exports = Categories
